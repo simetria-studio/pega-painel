407cb76ce2a2ba555b3b5254c88dac5b06995421
Felipe Cruz <fecruz2022@gmail.com>
2023-02-28 16:08:01 -0300

    4124

--- resources/views/dashboard/animais/index.blade.php
+++ resources/views/dashboard/animais/index.blade.php
@@ -9,7 +9,7 @@
         <div class="row justify-content-center">
             <div class="col-8 d-flex justify-content-evenly">
                 <div>
-                    <a role="button" type="button" href="{{ route('consultar-animais') }}" class="btn btn-animais ">
+                    <a role="button" type="button" href="{{ route('consultar.animais') }}" class="btn btn-animais ">
                         <iconify-icon icon="cil:magnifying-glass" width="100" height="100"></iconify-icon>
                         Consultar
                     </a>
