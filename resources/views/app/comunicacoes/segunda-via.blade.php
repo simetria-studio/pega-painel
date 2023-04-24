a0a02c51f218fb93a849e35f03026ac16c1cfbdf
Felipe Cruz <fecruz2022@gmail.com>
2023-04-24 14:26:31 -0300

    dawfa

--- resources/views/app/comunicacoes/segunda-via.blade.php
+++ resources/views/app/comunicacoes/segunda-via.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="my-8">
         <div class="m-4 text-red-900">
-            <nuxt-link @click="goBack">
+            <a href="{{ route('app.comunicacoes') }}">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-8 h-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                 </svg>
-            </nuxt-link>
+            </a>
         </div>
         <div class="text-center mb-5">
             <h1 class="text-xl "> Solicitar segunda via <br /> de registro animal </h1>
--- resources/views/app/comunicacoes/transferencia.blade.php
+++ resources/views/app/comunicacoes/transferencia.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="my-8">
         <div class="m-4 text-red-900">
-            <nuxt-link @click="goBack">
+            <a href="{{ route('app.comunicacoes') }}">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-8 h-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                 </svg>
-            </nuxt-link>
+            </a>
         </div>
         <div class="text-center my-8">
             <h1 class="text-center font-semibold text-xl">Comunicação de transferência de propriedade</h1>
